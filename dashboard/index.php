713602793ddab70c5a36973c17d33a3f75097b5a
kissdev <yscript@gmail.com>
2014-04-17 18:20:00 +0800

    added very cool metro-style dashboard

---
+++ dashboard/index.php
@@ -0,0 +1,176 @@
+<?php header("X-UA-Compatible: IE=8"); ?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="target-densitydpi=device-dpi, width=device-width, initial-scale=1.0, maximum-scale=1">
+	<link href="css/modern.css" rel="stylesheet">
+    <link href="css/modern-responsive.css" rel="stylesheet">
+<style>
+.page {
+width: 940px;
+margin: auto;
+background-color: #fff;
+}
+</style>
+</head>
+<body class="modern-ui" style="margin:0px;padding:0px">
+<div class="page">
+<div class="tiles clearfix">
+<h3>欢迎来到IT管家!</h3>
+<div class="tile image">
+	<div class="tile-content">
+    <img alt="" src="images/myface.jpg">
+    </div>
+</div>
+
+<div class="tile icon">
+	<div class="tile-content">
+    	<img src="images/Mail128.png">
+    </div>
+    <div class="brand">
+    	<div class="badge">10</div>
+    	<div class="name">信息</div>
+    </div>
+</div>
+
+<div class="tile bg-color-orangeDark icon selected">
+<b class="check"></b>
+	<div class="tile-content">
+		<img alt="" src="images/Video128.png">
+	</div>
+    <div class="brand">
+    	<span class="name">视频</span>
+    </div>
+</div>
+
+<div class="tile double image">
+	<div class="tile-content">
+    <img alt="" src="images/5.jpg">
+    </div>
+    <div class="brand">
+    	<span class="name">图片</span>
+        <div class="badge bg-color-orange">5</div>
+    </div>
+</div>
+
+<div class="tile double-vertical bg-color-yellow icon">
+	<div class="tile-content">
+		<img src="images/Calendar128.png">
+    </div>
+    <div class="brand">
+        <span class="name">日历</span>
+    </div>
+</div>
+
+                    <div class="tile bg-color-green icon selected">
+                        <b class="check"></b>
+                        <div class="tile-content">
+                            <img src="images/Market128.png">
+                        </div>
+                        <div class="brand">
+                            <span class="name">Store</span>
+                            <span class="badge">6</span>
+                        </div>
+                    </div>
+
+                    <div class="tile bg-color-red icon selected">
+                        <div class="tile-content">
+                            <img alt="" src="images/Music128.png">
+                        </div>
+                        <div class="brand">
+                            <span class="name">Music</span>
+                        </div>
+                    </div>
+
+                    <div class="tile double bg-color-blueDark">
+                        <div class="tile-content">
+                            <img class="place-left" src="images/michael.jpg">
+                            <h3 style="margin-bottom: 5px;">michael_angiulo</h3>
+                            <p>
+                                I just saw Thor last night. It was awesome! I think you'd love it.
+                                </p><h5>RT @julie_green</h5>
+                            <p></p>
+                        </div>
+                        <div class="brand">
+                            <span class="name">Tweet@rama</span>
+                        </div>
+                    </div>
+
+                    <div class="tile bg-color-darken icon">
+                        <div class="tile-content">
+                            <img alt="" src="images/YouTube128.png">
+                        </div>
+                        <div class="brand">
+                            <span class="name">YouTube</span>
+                        </div>
+                    </div>
+
+                    <div class="tile icon">
+                        <div class="tile-content">
+                            <img src="images/excel2013icon.png">
+                        </div>
+                        <div class="brand">
+                            <span class="name">Excel 2013</span>
+                        </div>
+                    </div>
+
+                    <div class="tile double bg-color-green">
+                        <div class="tile-content">
+                            <h2>mattberg@live.com</h2>
+                            <h5>Re: Wedding Annoucement!</h5>
+                            <p>
+                                Congratulations! I'm really excited to celebrate with you all. Thanks for...
+                            </p>
+                        </div>
+                        <div class="brand">
+                            <div class="badge">12</div>
+                            <img src="images/Mail128.png" class="icon">
+                        </div>
+                    </div>
+
+                    <div class="tile double image">
+                        <div class="tile-content">
+                            <img alt="" src="images/4.jpg">
+                        </div>
+                        <div class="brand bg-color-orange">
+                            <img src="images/Rss128.png" class="icon">
+                            <p class="text">This is a desert eagle. He is very hungry and angry bird.</p>
+                            <div class="badge">10</div>
+                        </div>
+                    </div>
+
+                    <div class="tile double image-set">
+                        <div class="tile-content">
+                            <img alt="" src="images/1.jpg">
+                        </div>
+                        <div class="tile-content">
+                            <img alt="" src="images/2.jpg">
+                        </div>
+                        <div class="tile-content">
+                            <img alt="" src="images/3.jpg">
+                        </div>
+                        <div class="tile-content">
+                            <img alt="" src="images/4.jpg">
+                        </div>
+                        <div class="tile-content">
+                            <img alt="" src="images/5.jpg">
+                        </div>
+                        <div class="brand">
+                            <span class="name">Photos</span>
+                        </div>
+                    </div>
+
+                    <div class="tile bg-color-blue icon">
+                        <div class="tile-content">
+                            <img src="images/InternetExplorer128.png">
+                        </div>
+                        <div class="brand">
+                            <span class="name">Internet Explorer</span>
+                        </div>
+                    </div>
+
+                </div>
+</div>
+</body>
+</html>
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -2,7 +2,161 @@
 /* @var $this SiteController */
 
 $this->pageTitle=Yii::app()->name;
+$cs=Yii::app()->getClientScript();
+$cs->registerCssFile('./css/modern.css');
+// $cs->registerCssFile('./css/modern-responsive.css');
 ?>
+<div class="row-fluid">
+<div style="width:940px">
+<!-- <div style="width: 940px;margin: auto;background-color: #fff;"> -->
+<!-- <iframe frameborder="0" width="970px" height="800px" scrolling="no" src="./dashboard/"></iframe> -->
+<div class="tiles clearfix">
+<h4>欢迎来到IT管家!</h4>
+<div class="tile image">
+	<div class="tile-content">
+    <img alt="" src="./images/myface.jpg">
+    </div>
+</div>
 
-<h3>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h3>
+<div class="tile icon">
+	<div class="tile-content">
+    	<img src="./images/Mail128.png">
+    </div>
+    <div class="brand">
+    	<div class="badge">10</div>
+    	<div class="name">信息</div>
+    </div>
+</div>
 
+<div class="tile bg-color-orangeDark icon">
+<b class="check"></b>
+	<div class="tile-content">
+		<img alt="" src="./images/Video128.png">
+	</div>
+    <div class="brand">
+    	<span class="name">视频</span>
+    </div>
+</div>
+
+<div class="tile double image">
+	<div class="tile-content">
+    <img alt="" src="images/5.jpg">
+    </div>
+    <div class="brand">
+    	<span class="name">图片</span>
+        <div class="badge bg-color-orange">5</div>
+    </div>
+</div>
+
+<div class="tile double-vertical bg-color-yellow icon">
+	<div class="tile-content">
+		<img src="images/Calendar128.png">
+    </div>
+    <div class="brand">
+        <span class="name">日历</span>
+    </div>
+</div>
+
+<div class="tile bg-color-green icon">
+	<b class="check"></b>
+    <div class="tile-content">
+		<img src="./images/Market128.png">
+    </div>
+    <div class="brand">
+		<span class="name">Store</span>
+    	<span class="badge">6</span>
+    </div>
+</div>
+
+<div class="tile bg-color-red icon">
+	<div class="tile-content">
+		<img alt="" src="./images/Music128.png">
+    </div>
+    <div class="brand">
+		<span class="name">Music</span>
+    </div>
+</div>
+
+<div class="tile double bg-color-blueDark">
+	<div class="tile-content">
+		<img class="place-left" src="./images/michael.jpg">
+        <h4 style="margin-bottom: 5px;">michael_angiulo</h4>
+        <p>我们改变世界，重新定义流量价值.</p>
+        <p></p>
+     </div>
+</div>
+
+<div class="tile bg-color-darken icon">
+	<div class="tile-content">
+	    <img alt="" src="./images/YouTube128.png">
+    </div>
+    <div class="brand">
+		<span class="name">YouTube</span>
+    </div>
+</div>
+
+<div class="tile icon">
+	<div class="tile-content">
+		<img src="./images/excel2013icon.png">
+	</div>
+	<div class="brand">
+		<span class="name">Excel 2013</span>
+    </div>
+</div>
+
+<div class="tile double bg-color-green">
+	<div class="tile-content">
+		<h3>mattberg@live.com</h3>
+        <h5>Re: Wedding Annoucement!</h5>
+        <p>从来都没有救世主...</p>
+    </div>
+    <div class="brand">
+		<div class="badge">12</div>
+        <img src="images/Mail128.png" class="icon">
+    </div>
+</div>
+
+<div class="tile double image">
+	<div class="tile-content">
+		<img alt="" src="./images/4.jpg">
+    </div>
+    <div class="brand bg-color-orange">
+		<img src="./images/Rss128.png" class="icon">
+		<p class="text">This is a desert eagle. He is very hungry and angry bird.</p>
+        <div class="badge">10</div>
+     </div>
+</div>
+
+					<div class="tile double image-set">
+                        <div class="tile-content">
+                            <img alt="" src="./images/1.jpg">
+                        </div>
+                        <div class="tile-content">
+                            <img alt="" src="./images/2.jpg">
+                        </div>
+                        <div class="tile-content">
+                            <img alt="" src="./images/3.jpg">
+                        </div>
+                        <div class="tile-content">
+                            <img alt="" src="./images/4.jpg">
+                        </div>
+                        <div class="tile-content">
+                            <img alt="" src="./images/5.jpg">
+                        </div>
+                        <div class="brand">
+                            <span class="name">Photos</span>
+                        </div>
+                    </div>
+
+                    <div class="tile bg-color-blue icon">
+                        <div class="tile-content">
+                            <img src="./images/InternetExplorer128.png">
+                        </div>
+                        <div class="brand">
+                            <span class="name">Internet Explorer</span>
+                        </div>
+                    </div>
+
+</div><!-- tiles -->
+</div><!-- page --->
+</div><!-- row-fluid -->
