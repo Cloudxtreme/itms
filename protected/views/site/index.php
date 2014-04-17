b44c20819b7494cb24baa1a561a741ae214b30f2
kissdev <yscript@gmail.com>
2014-04-18 04:30:38 +0800

    small improvement of metro dashboard

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,6 +4,8 @@
 $this->pageTitle=Yii::app()->name;
 $cs=Yii::app()->getClientScript();
 $cs->registerCssFile('./css/modern.css');
+$cs->registerScriptFile('./js/tile-slider.js');
+
 // $cs->registerCssFile('./css/modern-responsive.css');
 ?>
 <div class="row-fluid">
@@ -18,7 +20,7 @@ $cs->registerCssFile('./css/modern.css');
     </div>
 </div>
 
-<div class="tile icon">
+<div class="tile icon" onclick="top.location.href='./?r=host'">
 	<div class="tile-content">
     	<img src="./images/Mail128.png">
     </div>
@@ -104,11 +106,16 @@ $cs->registerCssFile('./css/modern.css');
     </div>
 </div>
 
-<div class="tile double bg-color-green">
+<div data-param-period="3000" data-role="tile-slider" class="tile double bg-color-green">
 	<div class="tile-content">
-		<h3>mattberg@live.com</h3>
+		<h4>mattberg@live.com</h4>
         <h5>Re: Wedding Annoucement!</h5>
         <p>从来都没有救世主...</p>
+    </div>
+   <div class="tile-content">
+        <h4>clickwise@admin.com</h4>
+        <h5>用户喜好永不违背!</h5>
+        <p>在愉悦中接受产品</p>
     </div>
     <div class="brand">
 		<div class="badge">12</div>
@@ -127,35 +134,35 @@ $cs->registerCssFile('./css/modern.css');
      </div>
 </div>
 
-					<div class="tile double image-set">
-                        <div class="tile-content">
-                            <img alt="" src="./images/1.jpg">
-                        </div>
-                        <div class="tile-content">
-                            <img alt="" src="./images/2.jpg">
-                        </div>
-                        <div class="tile-content">
-                            <img alt="" src="./images/3.jpg">
-                        </div>
-                        <div class="tile-content">
-                            <img alt="" src="./images/4.jpg">
-                        </div>
-                        <div class="tile-content">
-                            <img alt="" src="./images/5.jpg">
-                        </div>
-                        <div class="brand">
-                            <span class="name">Photos</span>
-                        </div>
-                    </div>
+<div data-param-period="3000" data-param-direction="left" data-role="tile-slider" class="tile double image-slider">
+	<div class="tile-content">
+	<img alt="" src="./images/1.jpg">
+	</div>
+    <div class="tile-content">
+    <img alt="" src="./images/2.jpg">
+    </div>
+    <div class="tile-content">
+    <img alt="" src="./images/3.jpg">
+    </div>
+    <div class="tile-content">
+    <img alt="" src="./images/4.jpg">
+    </div>
+    <div class="tile-content">
+    <img alt="" src="./images/5.jpg">
+    </div>
+    <div class="brand">
+    <span class="name">Photos</span>
+    </div>
+</div>
 
-                    <div class="tile bg-color-blue icon">
-                        <div class="tile-content">
-                            <img src="./images/InternetExplorer128.png">
-                        </div>
-                        <div class="brand">
-                            <span class="name">Internet Explorer</span>
-                        </div>
-                    </div>
+<div class="tile bg-color-blue icon">
+	<div class="tile-content">
+	<img src="./images/InternetExplorer128.png">
+	</div>
+	<div class="brand">
+	<span class="name">Internet Explorer</span>
+	</div>
+</div>
 
 </div><!-- tiles -->
 </div><!-- page --->
